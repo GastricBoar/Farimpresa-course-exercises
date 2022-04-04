79326f2850ccbdcf573b9d3f55c469483c6d260e
Daniele Ocello <79748663+GastricBoar@users.noreply.github.com>
2022-04-04 10:02:53 +0200

    Add files via upload

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //START variable declaration
+            int i, j; 
+            string orderButton;
+            Random rndNum = new Random();
+            //END variable declaration
+
+            //acquire array size from user input
+            Console.WriteLine("Hello, let's start. First off, insert the desired size for your array:\n");
+            int size = Convert.ToInt32(Console.ReadLine()); 
+            int[] arr = new int[size];
+
+            Console.WriteLine("\nHere's your initial array, filled with random generated numbers:");
+
+            //fill array with random numbers
+            for (i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rndNum.Next(1000); //generate random numbers, with max cap at 1000               
+            }
+
+            //display initial array
+            for (i = 0; i < size; i++)
+            {
+               Console.WriteLine(arr[i]);
+            }
+
+            Console.WriteLine("\nPress > to display array in descending order, or < to view it in ascending order:");
+            orderButton = Console.ReadLine();
+
+
+            if (orderButton == "<") //ascending order
+            {
+                for (i = 1; i < arr.Length; ++i) //i starts from 1 leaving space for variable j
+                {
+                    int key = arr[i]; //key declaration, separates sorted side of the array from unsorted 
+                    j = i - 1; //j variable, separates  
+                    
+                    while (j >= 0 && arr[j] > key) //while j >= 0, shift all elements to the right space, making space for the number  
+                    {
+                        arr[j + 1] = arr[j]; //moves element to the right side
+                        j = j - 1; //
+                    }
+                    arr[j + 1] = key; //sets the number to order in place 
+                }
+
+                Console.WriteLine("------------------------------------------------\nHere's your array sorted in ascending order: ");
+
+                for (i = 0; i < size; i++) //this loop outputs every element of the sorted array
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+            }
+
+            else if (orderButton == ">") //descending order part, does the same thing the code before does, but operator < is switched to > inside while loop
+            {
+                for (i = 1; i < arr.Length; ++i) 
+                {
+                    int key = arr[i]; 
+                    j = i - 1; 
+
+                    
+                    while (j >= 0 && arr[j] < key) 
+                    {
+                        arr[j + 1] = arr[j]; 
+                        j = j - 1; 
+                    }
+                    arr[j + 1] = key;
+                }
+
+                Console.WriteLine("------------------------------------------------\nHere's your array sorted in descending order: ");
+
+                for (i = 0; i < size; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ LoginChecks/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace LoginChecks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //START variable declaration
+            string email;
+            string password;
+            const string mailErrorMessage = "Email is not valid!\nRemember:\n + Email must contain @\n + Email name must be 3 characters or longer\n + Email domain must end in .it";
+            const string passwordErrorMessage = "Password is not valid!\nRemember:\n + Password must be longer than 10 characters\n + Password must contain at least one number, one uppercase character and one special character";
+            char[] numbers = "0123456789".ToCharArray();
+            char[] chars = "!£$%&/()=?^*°§#".ToCharArray();
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            bool isLengthAccepted = false;
+            bool containsNum = false;
+            bool containsChar = false;
+            bool containsUppercase = false;
+            string output;
+            
+            
+            //END variable declaration
+
+
+            Console.Write("Hello, to login please insert your email: ");
+            email = Console.ReadLine(); //acquire input email
+
+            //START email check
+            if (email.Contains("@")) //check for @ with .contains() method
+            {
+                string[] nameAndDomain = email.Split("@"); //email is split into username (index 0 of the array) and domain (index 1) using @ as separator
+               
+                output = nameAndDomain[0].Length >= 3 && nameAndDomain[1].EndsWith(".it") ? "Email is valid" : mailErrorMessage;
+
+                Console.WriteLine(output);
+
+            }
+            else
+            {
+                Console.WriteLine(mailErrorMessage);
+            }
+            //END email check
+
+            Console.WriteLine("\nNow insert your password: ");
+
+            password = Console.ReadLine(); //acquire input password
+
+            //START password check
+            if (password.Length > 10) { isLengthAccepted = true; } //password must be longer than 10 characters, condition is met for length > 10
+            
+            for (int i = 0; i < numbers.Length; i++) //password must contain at least one number, for loop iterates through the elements
+            {
+                if (password.Contains(numbers[i]) == true) //each character of password is compared to each number of the number array
+                {
+                    containsNum = true; //if condition is met, boolean is true 
+                }
+            }
+
+            for (int i = 0; i < chars.Length; i++) //password must contain special character, same procedure as above
+            {
+                if (password.Contains(chars[i]) == true)
+                {
+                    containsChar = true;
+                }
+            }
+
+            for (int i = 0; i < letters.Length; i++) //password must contain at least one uppercase letter, same procedure as above
+            {
+                if (password.Contains(letters[i]) == true)
+                {
+                    containsUppercase = true;
+                }
+            }
+
+            //if all condition specified are true, login is completed, else password is rejected
+            output = containsChar == true && containsUppercase == true && containsNum == true && isLengthAccepted == true ? "\nLogin completed, welcome user!" : passwordErrorMessage;
+
+        }
+
+
+        public bool GetEmail()
+        {
+            string email;
+            bool emailIsCorrect;
+            
+
+            Console.Write("Hello, to login please insert your email: ");
+            email = Console.ReadLine(); //acquire input email
+
+            if (email.Contains("@")) //check for @ with .contains() method
+            {
+                string[] nameAndDomain = email.Split("@"); //email is split into username (index 0 of the array) and domain (index 1) using @ as separator
+
+                emailIsCorrect = nameAndDomain[0].Length >= 3 && nameAndDomain[1].EndsWith(".it") ? true : false;
+
+                return emailIsCorrect;
+
+                
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }
+
+
+
+
+
+        
+    }
+}
