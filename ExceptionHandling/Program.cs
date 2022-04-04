bafb5fc99db1a9a69b56fc13a971f38826924fa4
Daniele Ocello <79748663+GastricBoar@users.noreply.github.com>
2022-04-04 09:59:48 +0200

    Add files via upload

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    /// <summary>
+    /// Manage Exception
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// tcp - http - https -> error 500: errore generico
+        /// 200 -> ok, tutto va bene
+        /// 404 not found
+        /// 400 bad request -> richiesta non accettata
+        /// 302 forbidden -> errore momentaneo
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            char op;
+            decimal res = 0;
+
+            //Inizio logica
+            try //prova a eseguire il codice dentro le parentesi
+            {
+                Console.WriteLine("Inserisci i due valori per eseguire le operazioni: ");
+                Console.WriteLine("Inserisci il primo numero: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Inserisci il secondo numero: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                res = (decimal)num1 / (decimal)num2;
+                Console.WriteLine("Il risultato è: " + res);
+            }
+            catch(FormatException formatException)
+            {
+
+            }            
+            catch (DivideByZeroException divideException) //Cattura l'errore, nel caso il server non risponde
+            {
+                //logerror(ex.message);
+
+                //Se sei entrato qui dentro, scrivi nel file di log il messaggio di errore, ma mostra all'utente un messaggio di attesa
+                Console.WriteLine("Ciao, potresti per piacere dividere il secondo numero inserendo un valore maggiore di zero?");
+
+                Console.WriteLine("Inserisci il secondo numero: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                if(num2 <= 0)
+                {
+                    Console.WriteLine("Ciao, potresti per piacere dividere il secondo numero inserendo un valore maggiore di zero?");
+
+                    Console.WriteLine("Inserisci il secondo numero: ");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            { //chiudi la connessione al database
+
+            }
+
+            //Fine logica
+        }
+    }
+}
---
+++ FindMinMaxValue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FindMinMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNum = new Random();
+            float[] data = new float[5];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = randomNum.Next(1000);
+            }
+
+            float min = data[0];
+            float max = data[0];
+
+            Console.WriteLine("Your random generated array is:\n");
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+
+            FindMinMax(data, ref min, ref max);
+            
+            Console.WriteLine("\nIl valore min trovato è " + min);
+            Console.WriteLine("Il valore max trovato è " + max);
+        }
+
+        private static void FindMinMax(float[] data, ref float min, ref float max)
+        {  
+
+            for (int i = 1; i < data.Length; i++) //loop cycles through data array, setting min and max values to data[0] allows to skip first cycle.
+            {
+
+                if (data[i] > max) //Compare data[0] with data[1], then if thisNum > max, max becomes new max, while min remains the smallest value.
+                {
+                    max = data[i];
+                }
+                else if (data[i] < min)
+                {
+                    min = data[i];
+                }
+            }
+
+        }
+
+
+    }
+}
