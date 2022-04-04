6710350d1155774ef0263322da0b7b56c5a32ce6
Daniele Ocello <79748663+GastricBoar@users.noreply.github.com>
2022-04-04 09:58:24 +0200

    Add files via upload

---
+++ CustomReplaceMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CustomReplaceMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase;
+            string valueContainer = string.Empty;
+            char oldChar, newChar;
+
+
+            Console.WriteLine("Hi! Please digit a phrase: ");
+
+            phrase = Console.ReadLine();
+
+
+            Console.WriteLine("Please specify which character you want to replace: ");
+
+            oldChar = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Please specify the character you want to replace: ");
+
+            newChar = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (phrase[i] == oldChar)
+                {
+                    newChar = phrase[i];
+                    valueContainer += newChar;
+                    continue;
+                }
+
+                valueContainer += phrase[i];
+            }
+
+            Console.WriteLine(valueContainer);
+        }
+    }
+}
---
+++ CustomSubstringMethod/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CustomSubstringMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int startIndex, lenght, i;
+            string subString = string.Empty;
+            string outputMessage;
+
+            Console.Write("Please insert your string: ");
+            message = Console.ReadLine();
+
+            Console.Write("Please insert the starting index for the substring: ");
+            startIndex = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please insert the lenght for the substring: ");
+            lenght = Convert.ToInt32(Console.ReadLine());
+            
+            outputMessage = SubString(message, startIndex, lenght);
+            Console.WriteLine(outputMessage);
+        }
+
+        public static string SubString(string message, int? startIndex, int lenght)
+        {
+
+            int i, finalIndex = 0;
+            string subString = string.Empty;
+
+            if (string.IsNullOrEmpty(message))
+            {
+                return "You have not inserted a message!";
+
+            }
+            else if(startIndex < 0 || lenght < 0 )
+            {
+                return "Start Index or lenght cannot be less than zero!";
+            }
+
+            int checkStartIndex = startIndex ?? 0;
+            finalIndex = lenght + checkStartIndex; //null coalescing, if var has value, pass that value, else pass user defined value (in this case, zero)
+
+
+
+            try
+            {
+              for (i = checkStartIndex; i < finalIndex; i++)
+              {
+                subString += message[i];
+              }
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                return(ex.Message);
+            }
+            
+
+            return subString;
+        }
+    }
+}
